fdd849b985929a3ef8ea8fdb10c2ac085371f790
Farah_H <farahmh@hotmail.co.uk>
2022-06-13 12:51:50 +0100

    correction

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = os.environ['mysql_host']
+app.config['MYSQL_HOST'] = os.environ['MYSQL_HOST']
 app.config['MYSQL_USER'] = os.environ['MYSQL_USER']
 app.config['MYSQL_PASSWORD'] = os.environ['MYSQL_PASSWORD']
 app.config['MYSQL_DB'] = 'users'
